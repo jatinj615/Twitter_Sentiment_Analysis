4719c35aa3119178d2c028e2b7c93bfff880d00f
Jatin <jatinj615@gmail.com>
2018-04-22 18:55:22 +0530

    sentiment analysis model

--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -4,10 +4,10 @@ import pandas as pd
 
 df = pd.read_csv('train_E6oV3lV.csv');
 X = df[['tweet']]
-y = df['label']
+y = pd.get_dummies(df['label']).values
 
 from keras.preprocessing.text import Tokenizer
-max_features = 8000
+max_features = 10000
 tokenzer = Tokenizer(num_words=max_features, filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~', split=' ', lower=True, )
 tokenzer.fit_on_texts(X['tweet'].values)
 X = tokenzer.texts_to_sequences(X['tweet'].values)
@@ -19,12 +19,16 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
 
 
-
 #import Keras
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
+from keras.layers import Embedding
+from keras.layers import SpatialDropout1D
+from keras.layers import LSTM
 
+
+# using ANN
 # Initialise ANN
 clf = Sequential()
 
@@ -49,6 +53,37 @@ with tf.device('/gpu:0'):
 y_pred = clf.predict(X_test)
 y_pred = (y_pred > 0.5)
 
+
+# Using RNN
+clf = Sequential()
+# adding First Embedded Layer
+clf.add(Embedding(max_features, 128, input_length=X.shape[1]))
+clf.add(SpatialDropout1D(0.2))
+
+# Adding Lstm Layer
+clf.add(LSTM(196, dropout=0.2, recurrent_dropout=0.2))
+clf.add(Dense(128, activation='relu'))
+#Adding output layer
+clf.add(Dense(2, activation='softmax'))
+
+# Compiling classifier
+clf.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+import tensorflow as tf
+with tf.device('/gpu:0'):
+    clf.fit(X_train, y_train, batch_size=32, epochs=10)
+
+y_pred = clf.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+from keras.models import model_from_json
+model_json = clf.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+clf.save_weights("model.h5")
+
+score, acc = clf.evaluate(X_test, y_test, batch_size=32)
 
 from sklearn.metrics import confusion_matrix, accuracy_score
 cm = confusion_matrix(y_test, y_pred)
