efdb0a6bfc51d3f19a237a23d6371b80d7f05abf
Jatin <jatinj615@gmail.com>
2018-04-28 16:16:28 +0530

    minor changes

--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -13,7 +13,7 @@ tokenzer.fit_on_texts(X['tweet'].values)
 X = tokenzer.texts_to_sequences(X['tweet'].values)
 
 from keras.preprocessing.sequence import pad_sequences
-X = pad_sequences(X)
+X = pad_sequences(X, maxlen=100)
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
@@ -28,42 +28,19 @@ from keras.layers import SpatialDropout1D
 from keras.layers import LSTM
 
 
-# using ANN
-# Initialise ANN
-clf = Sequential()
-
-# Adding the input layer and the first hidden layer
-clf.add(Dense(output_dim = 12, kernel_initializer = 'uniform', activation='relu', input_dim = 39))
-clf.add(Dropout(rate=0.1))
-
-# Adding Second Hidden layer
-clf.add(Dense(output_dim = 6, kernel_initializer = 'uniform', activation='relu'))
-clf.add(Dropout(rate=0.1))
-
-# Adding Output layer
-clf.add(Dense(output_dim = 1, kernel_initializer = 'uniform', activation='sigmoid')) #use suftmax function in more than two categories
-
-# Compiling ANN
-clf.compile(optimizer= 'adam', loss='binary_crossentropy', metrics = ['accuracy']) # In case of more than two categories loss function equals 'categorical_crossentropy'
-
-import tensorflow as tf
-with tf.device('/gpu:0'):
-    clf.fit(X_train, y_train, batch_size=32, epochs=10)
-
-y_pred = clf.predict(X_test)
-y_pred = (y_pred > 0.5)
-
-
 # Using RNN
 clf = Sequential()
-# adding First Embedded Layer
+# Adding First Embedded Layer
 clf.add(Embedding(max_features, 128, input_length=X.shape[1]))
 clf.add(SpatialDropout1D(0.2))
 
 # Adding Lstm Layer
 clf.add(LSTM(196, dropout=0.2, recurrent_dropout=0.2))
+
+# Adding fully connected layer
 clf.add(Dense(128, activation='relu'))
-#Adding output layer
+
+# Adding output layer
 clf.add(Dense(2, activation='softmax'))
 
 # Compiling classifier
@@ -73,8 +50,6 @@ import tensorflow as tf
 with tf.device('/gpu:0'):
     clf.fit(X_train, y_train, batch_size=32, epochs=10)
 
-y_pred = clf.predict(X_test)
-y_pred = (y_pred > 0.5)
 
 from keras.models import model_from_json
 model_json = clf.to_json()
@@ -83,11 +58,27 @@ with open("model.json", "w") as json_file:
 # serialize weights to HDF5
 clf.save_weights("model.h5")
 
-score, acc = clf.evaluate(X_test, y_test, batch_size=32)
+# loading model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
+loaded_model.compile(optimizer= 'adam', loss='binary_crossentropy', metrics = ['accuracy']) # In case of more than two categories loss function equals 'categorical_crossentropy'
 
+y_pred = loaded_model.predict(X_test)
+y_pred = (y_pred > 0.5)
+y_pred = y_pred[:, 1]
+score, acc = loaded_model.evaluate(X_test, y_test, batch_size=32)
+
+y_test = y_test[:, 1]
 from sklearn.metrics import confusion_matrix, accuracy_score
 cm = confusion_matrix(y_test, y_pred)
-acc = accuracy_score(y_test, y_pred)
 
+df_test = pd.read_csv('test_tweets_anuFYb8.csv')
+tweets = df_test['tweet']
 
-df = pd.read_csv('test_tweets_anuFYb8.csv')
+tweets = tokenzer.texts_to_sequences(tweets.values)
+tweets = pad_sequences(tweets)
+predicted = loaded_model.predict(tweets)
